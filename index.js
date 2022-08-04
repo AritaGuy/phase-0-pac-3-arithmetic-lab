d13e189c7cfc7b9dfe781488fe95eb8aa3a712f8
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-04 04:58:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+    let addition = a + b
+    return addition;
+}
+
+function subtract(a, b){
+    let subtraction = a - b
+    return subtraction;
+}
+
+function multiply(a, b){
+    let multiplication =  a * b
+    return multiplication;
+}
+
+function divide(a, b){
+    let division = a/b
+    return division;
+}
+
+function increment(n){
+    let incre = n+1
+    return incre;
+} 
+
+function decrement (n){
+    let decre = n-1
+    return decre;
+}
+
+function makeInt(n){
+    let convert = parseInt(n, 10);
+    return convert;
+}
+
+function preserveDecimal(n){
+    let convert2 = parseFloat(n)
+    return convert2;
+}
